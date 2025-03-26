3699a4069048835eaed32df88c52ce0728cc4d9d
Arunkumar Kamatchi <arunkumarkamatchi94@gmail.com>
2025-03-27 00:50:30 +0530

    Build URL Shortener Project setup
    I have created a new Spring Boot project for a URL shortener and configured the required Maven dependencies in the pom.xml.
    
    I am using Thymeleaf for the UI in the URL Shortener application. I have designed the layout for the application and configured it in the index.html page.

---
+++ src/main/java/com/studioone/urlshortener/StudioOneURLshortenerApplication.java
@@ -0,0 +1,13 @@
+package com.studioone.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioOneURLshortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioOneURLshortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studioone/urlshortener/StudioOneUrLshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studioone.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioOneURLshortenerApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
